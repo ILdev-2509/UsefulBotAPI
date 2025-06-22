745f3e7b506b4fd1390830e38165d6f145be04a4
ILdev <l.i.d.music2008@gmail.com>
2025-06-23 02:44:36 +0300

    добавил сервис перевода текста с кода морзе на русский

--- app/service/translators/morse.py
+++ app/service/translators/morse.py
@@ -26,3 +26,32 @@ class ToMorseTranslator(BaseTranslator):
             result.append(self._dictionary.get(char, char))
         return ' '.join(result)
 
+class FromMorseTranslator(BaseTranslator):
+    _reverse_dictionary_cache: dict[str, str] = None
+
+    def __init__(self):
+        decoder = JSONDecoder()
+        if FromMorseTranslator._reverse_dictionary_cache is None:
+            chardict = decoder.decode(get_path("translator/morse", "russian_morse.json")) or {}
+            symboldict = decoder.decode(get_path("translator/morse", "symbols.json")) or {}
+            merged = chardict | symboldict
+            FromMorseTranslator._reverse_dictionary_cache = {v: k for k, v in merged.items()}
+        self._dictionary = FromMorseTranslator._reverse_dictionary_cache
+
+    def translate(self, text: str) -> str:
+        """
+        Переводит код морзе обратно в текст
+        Args:
+            text (str): код морзе для перевода
+        Returns:
+            str: текст, переведенный с кода морзе
+        """
+        words = text.strip().split(' / ')
+        decoded_words = []
+
+        for word in words:
+            chars = word.strip().split()
+            decoded_word = ''.join([self._dictionary.get(c, c) for c in chars])
+            decoded_words.append(decoded_word)
+
+        return ' '.join(decoded_words)
