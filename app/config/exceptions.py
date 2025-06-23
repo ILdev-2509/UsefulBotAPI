3167eeaffc8659c434cef582d41db38453ae58df
ILdev <l.i.d.music2008@gmail.com>
2025-06-23 03:13:37 +0300

    подготовил два класса исключений и добавил недостающие __init__.py

---
+++ app/config/exceptions.py
@@ -0,0 +1,6 @@
+class TooBigBaseException(Exception):
+    """будет вызвано, если основание системы больше, чем 36"""
+
+class TooSmallBaseException(Exception):
+    """будет вызвано, если основание системы меньше, чем 2"""
+
